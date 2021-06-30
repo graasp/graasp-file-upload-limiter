1a07cb1194e0c7b96ec769549bd7a6d63cd88567
pyphilia <pyphilia@gmail.com>
2021-06-30 11:32:55 +0200

    refactor: compute file size sum in sql

--- index.ts
+++ index.ts
@@ -7,7 +7,9 @@ const DEFAULT_MAX_STORAGE = 1024 * 1024 * 100; // 100MB;
 interface GraaspFileUploadLimiterOptions {
   /** Item type to target (ex: 'file', 's3File') */
   type: string;
-  /** Chain of property names to the file size (ex: 'extra.s3File.size')  */
+  /** Chain of property names to the file size (ex: 's3File.size')
+   * size must be defined in extra
+   */
   sizePath: string;
   /** Maximum storage size for a user in bytes (default to 100MB)  */
   maxMemberStorage?: number;
@@ -30,7 +32,6 @@ const plugin: FastifyPluginAsync<GraaspFileUploadLimiterOptions> = async (fastif
   const {
     items: { taskManager },
     taskRunner: runner,
-    db,
   } = fastify;
   const { type: itemType, sizePath, maxMemberStorage = DEFAULT_MAX_STORAGE } = options;
 
@@ -38,9 +39,9 @@ const plugin: FastifyPluginAsync<GraaspFileUploadLimiterOptions> = async (fastif
     throw new Error("graasp-file-upload-limiter: missing plugin options");
   }
 
-  const getFileSize = (item): number => {
+  const getFileSize = (extra): number => {
     const properties = sizePath.split(".");
-    const sizeField = properties.reduce((prev, curr) => prev?.[curr], item);
+    const sizeField = properties.reduce((prev, curr) => prev?.[curr], extra);
 
     if (Number.isInteger(sizeField)) {
       return sizeField;
@@ -49,45 +50,36 @@ const plugin: FastifyPluginAsync<GraaspFileUploadLimiterOptions> = async (fastif
     throw new Error("sizePath does not lead to a size number");
   };
 
-  const getMemberStorage = (id: string) => {
+  const getMemberStorage = (id: string, handler): Promise<number> => {
     // todo: fetch total storage from Redis
 
-    // -- the following solution is failing because it cannot extract property from jsonb
-    // const column = sql.join(["extra", "file", "size"], sql`->`);
-    // return db.pool
-    //   .oneFirst<string>(
-    //     sql`
-    //        SELECT
-    //        SUM((${column})) as size
-    //        FROM item
-    //        WHERE item.type ='file' AND item.creator = ${id}`
-    //   )
-    //   .then((size) => parseInt(size, 10) || 0);
-
     // select all file extra for given member
+    const properties = sizePath.split(".");
+    const propertiesPath = sql.join(properties, sql`->`);
     return (
-      db.pool
-        .query<any>(
+      handler
+        .query(
           sql`
-            SELECT extra
+            SELECT 
+            SUM((extra->${propertiesPath})::int)
             FROM item 
             WHERE item.type =${itemType} AND item.creator = ${id}`
         )
         // sum up
-        .then(({ rows }) => rows.reduce((acc, item) => getFileSize(item) + acc, 0))
+        .then(({ rows }) => rows[0].sum)
     );
   };
 
   // check the user has enough storage to create a new item given its size
   // get the complete storage
-  const checkRemainingStorage: PreHookHandlerType<Item> = async (item, actor) => {
+  const checkRemainingStorage: PreHookHandlerType<Item> = async (item, actor, { handler }) => {
     // enabled only on given item type
     if (item.type !== itemType) return;
 
-    const size = getFileSize(item);
+    const size = getFileSize(item.extra);
 
     const { id: memberId } = actor as Member<MemberExtra>;
-    const currentStorage = await getMemberStorage(memberId);
+    const currentStorage = await getMemberStorage(memberId, handler);
 
     if (currentStorage + size > maxMemberStorage) {
       throw new StorageExceeded();
